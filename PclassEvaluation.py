86c6b37c6733042ea6e88a435de22c7e05052984
Sepehr Liasi <sepehrghaseminejad@yahoo.com>
2024-12-09 16:06:55 +0330

    Pclass evaluation updated

--- PclassEvaluation.py
+++ PclassEvaluation.py
@@ -1,7 +1,5 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib.ticker import PercentFormatter
 
 train_data = pd.read_csv('train.csv')
 class_list = train_data[['PassengerId', 'Survived', 'Pclass']]
@@ -10,14 +8,11 @@ survived = class_list[class_list['Survived'].isin([1])]
 died = class_list[class_list['Survived'].isin([0])]
 print(class_list.info())
 
-# print(age_list)
 plt.figure(figsize=(10, 4))
 
-plt.hist(survived['Pclass'], color='green', weights=np.ones(len(survived)) / len(survived), alpha=0.7, edgecolor = 'yellow')
-plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
+plt.hist(survived['Pclass'], bins=[1,2,3,4], rwidth=0.5, color='green', alpha=0.7, edgecolor='yellow')
 
-plt.hist(died['Pclass'], color='red', weights=np.ones(len(died)) / len(died), alpha=0.5)
-plt.gca().yaxis.set_major_formatter(PercentFormatter(1))
+plt.hist(died['Pclass'], bins=[1,2,3,4], rwidth=0.6, color='red', alpha=0.5)
 
 plt.title('Died & Survived Pclass distribution')
 
