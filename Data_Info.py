7878f8d0e757a9f5c5c648d7988adc76ea5dcd25
Sepehr Liasi <sepehrghaseminejad@yahoo.com>
2025-01-11 20:45:13 +0330

    minor display bug fixed

--- Data_Info.py
+++ Data_Info.py
@@ -14,14 +14,14 @@ print(df)
 for item in df.itertuples():
     if pandas.isnull(item[2]):
         if item[1] == 1:
-            survived_cabin += 1
-        else:
-            dead_cabin += 1
-    else:
-        if item[1] == 1:
             s += 1
         else:
             d += 1
+    else:
+        if item[1] == 1:
+            survived_cabin += 1
+        else:
+            dead_cabin += 1
 
 print('survived cabin:', survived_cabin / (survived_cabin+dead_cabin))
 print('survived regular:', s / (s+d))
